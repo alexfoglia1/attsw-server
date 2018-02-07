be299e56c9a3e2f49ca8c9e0efdc940cd9b0702b
Alex Foglia <alex.f1793@gmail.com>
2018-02-07 20:50:20 +0100

    created gridservicetest class

---
+++ server/src/test/java/com/alexfoglia/server/GridServiceTest.java
@@ -0,0 +1,14 @@
+package com.alexfoglia.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GridServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
