5274ccd4649890c68c82530f22bbc44870c40175
Alex Foglia <alex.f1793@gmail.com>
2018-02-08 04:56:34 +0100

    generated equals used for webcontroller test

--- server/src/main/java/com/alexfoglia/server/DatabaseGrid.java
+++ server/src/main/java/com/alexfoglia/server/DatabaseGrid.java
@@ -1,8 +1,39 @@
 package com.alexfoglia.server;
 
+import java.util.Arrays;
+
 import org.springframework.data.annotation.Id;
 
 public class DatabaseGrid {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + Arrays.deepHashCode(matrix);
+		result = prime * result + n;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DatabaseGrid other = (DatabaseGrid) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (!Arrays.deepEquals(matrix, other.matrix))
+			return false;
+		if (n != other.n)
+			return false;
+		return true;
+	}
 	@Id
 	private String id;
 	private int[][] matrix;
