fce1bf34440c62c83d97ef32a31d0b171a03eda4
Alex Foglia <alex.f1793@gmail.com>
2018-02-08 01:46:21 +0100

    first test pass (status 200)

--- server/src/main/java/com/alexfoglia/server/ARestController.java
+++ server/src/main/java/com/alexfoglia/server/ARestController.java
@@ -1,5 +1,13 @@
 package com.alexfoglia.server;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ARestController {
+	@GetMapping("/api")
+	public String index() {
+		return "";
+	}
 
 }
