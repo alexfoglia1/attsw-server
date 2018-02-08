fd70c436b4ef28f734c4f1ead3b8d5d65e925209
Alex Foglia <alex.f1793@gmail.com>
2018-02-08 01:55:25 +0100

    implemented getAllGrids in api

--- server/src/main/java/com/alexfoglia/server/ARestController.java
+++ server/src/main/java/com/alexfoglia/server/ARestController.java
@@ -1,13 +1,26 @@
 package com.alexfoglia.server;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 @RestController
 public class ARestController {
+	@Autowired
+	private IGridService service;
 	@GetMapping("/api")
 	public String index() {
-		return "";
+		Gson serializer=new Gson();
+		List<DatabaseGrid> grids=service.findAllGridsInDb();
+		List<String> allids= new LinkedList<String>();
+		grids.forEach(grid->allids.add(grid.getId()));
+		return serializer.toJson(allids);
+		
 	}
 
 }
--- server/src/test/java/com/alexfoglia/server/RestControllerTest.java
+++ server/src/test/java/com/alexfoglia/server/RestControllerTest.java
@@ -1,7 +1,18 @@
 package com.alexfoglia.server;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +20,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.test.web.servlet.MvcResult;
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers=ARestController.class)
 @Import(WebSecurityConfig.class)
@@ -25,4 +37,31 @@ public class RestControllerTest {
 	public void testStatus200() throws Exception{
 		mvc.perform(get("/api")).andExpect(status().isOk());
 	}
+	@Test
+	public void testGetAllIdsWhenNoGridExists() throws Exception {
+		whenGetAllGridsReturn(gridService,new LinkedList<DatabaseGrid>());
+		this.mvc.perform(get("/api")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$",hasSize(0)));
+				
+			verify(gridService,times(1)).findAllGridsInDb();
+		
+	}
+	private void whenGetAllGridsReturn(IGridService gs,List<DatabaseGrid> toreturn) {
+		given(gs.findAllGridsInDb()).willReturn(toreturn);
+	}
+	@Test
+	public void testApiWhenSingleGridExists() throws Exception {
+		DatabaseGrid toreturn=new DatabaseGrid();
+		toreturn.setId("0");
+		whenGetAllGridsReturn(gridService,Arrays.asList(toreturn));
+		this.mvc.perform(get("/api")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0]",is("0")));
+				
+			verify(gridService,times(1)).findAllGridsInDb();
+	}
+	
 }
