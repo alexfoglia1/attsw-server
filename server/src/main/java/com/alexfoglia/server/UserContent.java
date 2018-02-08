4c0de92d8a567edcc3927160d93b029d8f5596cf
Alex Foglia <alex.f1793@gmail.com>
2018-02-08 13:23:50 +0100

    created wrapper class for parsing user form input

---
+++ server/src/main/java/com/alexfoglia/server/UserContent.java
@@ -0,0 +1,35 @@
+package com.alexfoglia.server;
+
+public class UserContent {
+	private int number;
+	private String content;
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int[][] parseMatrix() {
+		int[][] matrix = new int[number][number];
+		int contiter = 0;
+		for (int i = 0; i < number; i++) {
+			for (int j = 0; j < number; j++) {
+				try {
+					matrix[i][j] = Integer.parseInt(String.format("%c", content.charAt(contiter++)));
+				} catch (Exception exc) {
+
+					matrix[i][j] = 0;
+				}
+			}
+		}
+		return matrix;
+	}
+	
+
+}
