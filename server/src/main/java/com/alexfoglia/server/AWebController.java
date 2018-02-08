7c830f9383bce4f2d6dbdd75638df72f67828569
Alex Foglia <alex.f1793@gmail.com>
2018-02-08 13:24:39 +0100

    implemented webcontroller->add table

--- server/src/main/java/com/alexfoglia/server/AWebController.java
+++ server/src/main/java/com/alexfoglia/server/AWebController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +24,25 @@ public class AWebController {
 		List<DatabaseGrid> allGrids = service.findAllGridsInDb();
 		model.addAttribute("allGrids", allGrids);
 		return "dbview";
+
+	}
+	@GetMapping("/addtable")
+	public String addtableForm(Model model) {
+		model.addAttribute(USERCONTENT, new UserContent());
+		return "tableadd";
+	}
+
+	@PostMapping("/addtable") 
+	public String addtable(@ModelAttribute UserContent content, Model mod) {
+		int n = content.getNumber();
+		if(n<0) {
+			mod.addAttribute(USERCONTENT,new UserContent());
+			mod.addAttribute("errormessage","Matrix size must be >= 0");
+			return "tableadd";
+		}
+		int[][] matrix=content.parseMatrix();
+		service.storeInDb(n,matrix);
+		return "redirect:/";
 
 	}
 }
--- server/src/test/java/com/alexfoglia/server/WebControllerTest.java
+++ server/src/test/java/com/alexfoglia/server/WebControllerTest.java
@@ -1,10 +1,17 @@
 package com.alexfoglia.server;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -15,17 +22,17 @@ import java.util.LinkedList;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers=AWebController.class)
 @Import(WebSecurityConfig.class)
@@ -92,5 +99,88 @@ public class WebControllerTest  {
 				andExpect(view().name("dbview")).
 				andExpect(model().attribute("allGrids", Arrays.asList(new DatabaseGrid(),new DatabaseGrid())));
 		verify(gridService,times(1)).findAllGridsInDb();
+	}
+	@Test
+	public void testAddTableRendering() throws Exception {
+		
+		mockMvc.perform(get("/addtable").with(httpBasic("user","password"))).andExpect(
+				status().isOk()).
+				andExpect(view().name("tableadd")).
+				andExpect(model().attribute("usercontent",isA(UserContent.class))).
+				andExpect(model().attributeDoesNotExist("errormessage"));
+	}
+	@Test
+	public void testAddOneTable() throws Exception {
+
+		mockMvc.perform(post("/addtable").with(httpBasic("user","password")).
+				param("content","1010").
+				param("number","2")).
+				andExpect(status().is3xxRedirection()).
+				andExpect(view().name("redirect:/"));
+
+		int[][] expmat=new int[][] {{1,0},{1,0}};
+		verifyInvokedStoreInDbWithArguments(2,expmat);
+		
+	}
+	@Test
+	public void testAddOneTableWhenMissingContent() throws Exception {
+		
+		mockMvc.perform(post("/addtable").with(httpBasic("user","password")).
+				param("content","1010").
+				param("number","3")).
+				andExpect(status().is3xxRedirection()).
+				andExpect(view().name("redirect:/"));
+		int[][] expmat=new int[][] {{1,0,1},{0,0,0},{0,0,0}};
+		verifyInvokedStoreInDbWithArguments(3,expmat);
+		
+	}
+	@Test
+	public void testAddOneTableWhenTooContent() throws Exception {
+	
+		mockMvc.perform(post("/addtable").with(httpBasic("user","password")).
+				param("content","11101010101010").
+				param("number","2")).
+				andExpect(status().is3xxRedirection()).
+				andExpect(view().name("redirect:/"));
+	
+		int[][] expmat=new int[][] {{1,1},{1,0}};
+		verifyInvokedStoreInDbWithArguments(2,expmat);
+		
+
+		
+	}
+	@Test
+	public void testAddOneTableWhenWrongContent() throws Exception {
+	
+		mockMvc.perform(post("/addtable").with(httpBasic("user","password")).
+				param("content","pippopluto").
+				param("number","2")).
+				andExpect(status().is3xxRedirection()).
+				andExpect(view().name("redirect:/"));
+;
+		int[][] expmat=new int[][] {{0,0},{0,0}};
+		verifyInvokedStoreInDbWithArguments(2,expmat);
+		
+	}
+	private void verifyInvokedStoreInDbWithArguments(int n, int[][] expmat) {
+		ArgumentCaptor<Integer> c1=ArgumentCaptor.forClass(Integer.class);
+		ArgumentCaptor<int[][]> c2=ArgumentCaptor.forClass(int[][].class);
+		verify(gridService,times(1)).storeInDb(c1.capture(),c2.capture());
+		assertEquals(n,c1.getValue().intValue());
+		assertArrayEquals(expmat,c2.getValue());
+		
+	}
+	@Test
+	public void testAddOneTableWhenWrongMatrixSize() throws Exception {
+	
+		mockMvc.perform(post("/addtable").with(httpBasic("user","password")).
+				param("content","").
+				param("number","-2")).
+				andExpect(status().is2xxSuccessful()).
+				andExpect(view().name("tableadd"))
+				.andExpect(model().attributeExists("errormessage"));
+
+		verify(gridService,times(0)).storeInDb(anyInt(),any(int[][].class));
+		
 	}
 }
