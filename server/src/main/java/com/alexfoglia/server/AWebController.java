2a7e370ef98367ec30e319dfef8b40dd4463cef6
Alex Foglia <alex.f1793@gmail.com>
2018-02-08 04:56:47 +0100

    implemented view database

--- server/src/main/java/com/alexfoglia/server/AWebController.java
+++ server/src/main/java/com/alexfoglia/server/AWebController.java
@@ -1,5 +1,7 @@
 package com.alexfoglia.server;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,5 +15,12 @@ public class AWebController {
 	@RequestMapping("/")
 	public String welcome(Model model) {
 		return "database";
+	}
+	@RequestMapping("/viewdb")
+	public String viewdb(Model model) {
+		List<DatabaseGrid> allGrids = service.findAllGridsInDb();
+		model.addAttribute("allGrids", allGrids);
+		return "dbview";
+
 	}
 }
--- server/src/test/java/com/alexfoglia/server/WebControllerTest.java
+++ server/src/test/java/com/alexfoglia/server/WebControllerTest.java
@@ -1,13 +1,21 @@
 package com.alexfoglia.server;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -58,5 +66,31 @@ public class WebControllerTest  {
 		mockMvc.perform(get("/").with(httpBasic("user","password"))).
 			andExpect(view().name("database"));
 			
+	}
+	@Test
+	public void testViewDb() throws Exception{
+		mockMvc.perform(get("/viewdb").with(httpBasic("user","password"))).andExpect(
+				status().isOk()).
+				andExpect(view().name("dbview")).
+				andExpect(model().attribute("allGrids", new LinkedList<>()));
+		verify(gridService,times(1)).findAllGridsInDb();
+	}
+	@Test
+	public void testViewDbWhenThereIsOneGrid() throws Exception{
+		when(gridService.findAllGridsInDb()).thenReturn(Arrays.asList(new DatabaseGrid()));
+		mockMvc.perform(get("/viewdb").with(httpBasic("user","password"))).andExpect(
+				status().isOk()).
+				andExpect(view().name("dbview")).
+				andExpect(model().attribute("allGrids", Arrays.asList(new DatabaseGrid())));
+		verify(gridService,times(1)).findAllGridsInDb();
+	}
+	@Test
+	public void testViewDbWhenThereAreMultipleGrids() throws Exception{
+		when(gridService.findAllGridsInDb()).thenReturn(Arrays.asList(new DatabaseGrid(),new DatabaseGrid()));
+		mockMvc.perform(get("/viewdb").with(httpBasic("user","password"))).andExpect(
+				status().isOk()).
+				andExpect(view().name("dbview")).
+				andExpect(model().attribute("allGrids", Arrays.asList(new DatabaseGrid(),new DatabaseGrid())));
+		verify(gridService,times(1)).findAllGridsInDb();
 	}
 }
