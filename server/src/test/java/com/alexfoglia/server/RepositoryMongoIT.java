fa18abedf9a1dea85f47be662a73179b524ebea4
Alex Foglia <alex.f1793@gmail.com>
2018-02-08 02:21:30 +0100

    created repositorytest

---
+++ server/src/test/java/com/alexfoglia/server/RepositoryMongoIT.java
@@ -0,0 +1,17 @@
+package com.alexfoglia.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RepositoryMongoIT {
+
+	@Autowired
+	private IMongoRepository repo;
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
