1a5ca47e25d1e2458931bb6f80e702ea42566462
Alex Foglia <alex.f1793@gmail.com>
2018-04-08 20:23:50 +0200

    e2e class created

---
+++ end2end/src/test/java/org/end2end/End2EndIT.java
@@ -0,0 +1,14 @@
+package org.end2end;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class End2EndIT {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
