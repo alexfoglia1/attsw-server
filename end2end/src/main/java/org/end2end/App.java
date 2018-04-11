9ce4ee4f98aaec282e0a567074ee08d534d7c8c6
tom <tommasopuccetti@gmail.com>
2018-04-12 01:00:25 +0200

    blank main

--- end2end/src/main/java/org/end2end/App.java
+++ end2end/src/main/java/org/end2end/App.java
@@ -1,12 +1,9 @@
 package org.end2end;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class App 
 {
 	public static void main( String[] args )
 	{
-		Logger.getGlobal().log(Level.ALL, "End To End Tests must be runned as Junit test cases", "");
+		//This method intentionally left blank
 	}
 }
