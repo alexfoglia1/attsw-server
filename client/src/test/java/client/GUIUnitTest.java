6390982d00ca4b0fd6f9e6bd07dadcc4ca9d5ac4
tom <tommasopuccetti@gmail.com>
2018-04-12 13:53:48 +0200

    organized imports

--- client/src/test/java/client/GUIUnitTest.java
+++ client/src/test/java/client/GUIUnitTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
